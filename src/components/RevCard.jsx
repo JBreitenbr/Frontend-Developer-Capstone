cde60be0075fedfe4258091df78ada9b681b666a
JBreitenbr <JuliaBreitenbruch@gmail.com>
2024-01-27 22:13:33 +0000

    cosmetics

--- src/components/RevCard.jsx
+++ src/components/RevCard.jsx
@@ -1,11 +1,11 @@
-/*import "../styles/RevCard.css";*/
+import "../styles/RevCard.css";
 const RevCard = ({ customer }) => {
   return (
-    <article className="testimonial-card">
-      <img src={customer.custImg} alt={customer.isCalled} />
-      <h4 style={{fontSize:"2rem"}} className="cust-name">{customer.isCalled}</h4>
-        <p style={{fontSize:"1.1rem"}}><i className="fa fa-quote-left"></i>&nbsp;&nbsp;{customer.custRev}&nbsp;&nbsp;<i className="fa fa-quote-right"></i></p>
-    </article>
+    <div className="test-div"><div className="test-wrapper">
+      <img src={customer.custImg} alt={customer.isCalled} className="test-pic" />
+      <h4 style={{fontSize:"2rem"}} className="cust-name">&nbsp;&nbsp;{customer.isCalled}</h4></div>
+        <p className="test-para"><i className="fa fa-quote-left"></i>&nbsp;&nbsp;{customer.custRev}&nbsp;&nbsp;<i className="fa fa-quote-right"></i></p>
+    </div>
   );
 };
 
--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -2,15 +2,12 @@ import "../styles/Reviews.css";
 import RevCard from "./RevCard";
 import customerData from "../helpers/customers";
 const Reviews = () => {
-  return (
-    <section className="testimonials">
-      <div className="container grid">
-        <h2>Testimonials</h2>
+  return (<article>
+    <div className="testis">
         {customerData.map((customer, index) => (
           <RevCard key={index} customer={customer} />
         ))}
-      </div>
-    </section>
+    </div></article>
   );
 };
 
