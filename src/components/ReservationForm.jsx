a32c85569065a60c45f8d307df10249279e86045
JBreitenbr <JuliaBreitenbruch@gmail.com>
2024-01-28 10:24:16 +0000

    simplified Reservations.jsx

--- src/components/ReservationForm.jsx
+++ src/components/ReservationForm.jsx
@@ -2,11 +2,10 @@ import { useState } from "react";
 import FormField from "./FormField";
 
 const ReservationForm = ({
-  availableTimes,
-  dispatchOnDateChange,
-  submitData,
+  submitData
 }) => {
   const minimumDate = new Date().toISOString().split("T")[0];
+  const availableTimes=["10:30","11:00","11:30" ,"12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00","16:30","17:00","17:30","18:00","18:30","19:00","19:30","20:00","20:30","21:00","21:30","22:00","22:30"];
   const defaultTime = availableTimes[0];
   const minimumNumberOfGuests = 1;
   const maximumNumberOfGuests = 10;
@@ -42,7 +41,6 @@ const myObject = {
 
   const handleDateChange = (e) => {
     setDate(e.target.value);
-    dispatchOnDateChange(e.target.value);
   };
 
   const handleTimeChange = (e) => setTime(e.target.value);
